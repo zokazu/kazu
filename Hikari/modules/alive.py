c2736d098e79e050a4a2036fd3f2d5f0794bdc5e
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-02-14 03:00:04 +0700

    Update alive.py

--- Hikari/modules/alive.py
+++ Hikari/modules/alive.py
@@ -13,12 +13,12 @@ PHOTO = "https://telegra.ph/file/1be0ff236e8c3de2772c4.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  PRIME = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Prime Mega.** \n\n"
+  PRIME = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Hikari Robot.** \n\n"
   PRIME += "🌼 **I'm Working Properly** \n\n"
-  PRIME += f"🌼 **My Master : [Lord](https://t.me/Bukan_guudlooking)** \n\n"
+  PRIME += f"🌼 **My Master : [ʀᴇxᴧ-ᴇx](https://t.me/JustRex)** \n\n"
   PRIME += f"🌼 **Library Version :** `{telever}` \n\n"
   PRIME += f"🌼 **Telethon Version :** `{tlhver}` \n\n"
   PRIME += f"🌼 **Pyrogram Version :** `{pyrover}` \n\n"
   PRIME += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/PrimeMegaBot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/PrimeSupportGroup")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/HikariMangeRobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/rexaprivateroom")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=PRIME,  buttons=BUTTON)
