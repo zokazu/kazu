0d1873e9dfb989a3a7372acbea8bcbb469d1fb8e
KAZU <99527274+kzmusic@users.noreply.github.com>
2022-03-08 22:22:10 +0700

    Update alive.py

--- Hikari/modules/alive.py
+++ Hikari/modules/alive.py
@@ -9,16 +9,16 @@ from Hikari.events import register
 from Hikari import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/1be0ff236e8c3de2772c4.jpg"
+PHOTO = "https://telegra.ph/file/3a0688ef26c90fdc706e7.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  PRIME = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Hikari Robot.** \n\n"
+  PRIME = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Kazu Robot.** \n\n"
   PRIME += "🌼 **I'm Working Properly** \n\n"
-  PRIME += f"🌼 **My Master : [ʀᴇxᴧ-ᴇx](https://t.me/JustRex)** \n\n"
+  PRIME += f"🌼 **My TUAN : [Ҡᴧƶᴜ](https://t.me/kazuinhere)** \n\n"
   PRIME += f"🌼 **Library Version :** `{telever}` \n\n"
   PRIME += f"🌼 **Telethon Version :** `{tlhver}` \n\n"
   PRIME += f"🌼 **Pyrogram Version :** `{pyrover}` \n\n"
   PRIME += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/HikariManageRobot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/rexaprivateroom")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/kaazuuBot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/punyasejutaumat")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=PRIME,  buttons=BUTTON)
